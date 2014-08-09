ef76e90bad065c60fb311cf59336ec51b7331b45
Allen Rose <Allen@TheRoseEffect.com>
2014-08-09 01:08:15 +0000

    Added assets and templates.

--- mediamanager/urls.py
+++ mediamanager/urls.py
@@ -1,12 +1,20 @@
-from django.conf.urls import patterns, include, url
-
+from django.conf import settings
+from django.conf.urls import include, patterns, url
 from django.contrib import admin
+
+from . import views
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mediamanager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+urlpatterns = patterns(
+    '',
+    url(r'^$', views.HomepageView.as_view(), name='home'),
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += patterns(
+    '',
+    (r'^static/(.*)$', 'django.views.static.serve', {
+        'document_root': settings.STATIC_ROOT,
+    })
 )
