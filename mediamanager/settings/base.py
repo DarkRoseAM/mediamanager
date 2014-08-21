6f2b062aa87e93f86657ab1f59bbad53ca87a8c3
Allen Rose <Allen@TheRoseEffect.com>
2014-08-21 14:21:38 +0000

    Removed AWS Global variables.

--- mediamanager/settings/base.py
+++ mediamanager/settings/base.py
@@ -95,8 +95,6 @@ USE_L10N = True
 USE_TZ = True
 
 # Amazon Web Service
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
 AWS_STORAGE_BUCKET_NAME = 'tremediamanager'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 
